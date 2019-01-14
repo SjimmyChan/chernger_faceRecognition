9caacb4024070e16faf967a60ed44c292666331a
Chernger <mmmmm123306@gmail.com>
2019-01-14 17:08:59 +0800

    fix takephoto.py bug

--- photo_shot.py
+++ photo_shot.py
@@ -1,19 +1,19 @@
 import cv2
 import sys
 
-def takeshot(self):
+def takeshot():
 
     cameraCapture = cv2.VideoCapture(0)
     success, frame = cameraCapture.read()
     count = 0
 
-    while(1):
-        count += 1
-        cv2.imshow("capture", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite("C:\Users\CN\Desktop\intern\chernger_faceRecognition\source-jimmy\jimmy" + str(count) + ".jpg", frame)
-        elif cv2.waitKey(1) & 0xFF == ord('x'):
-            break
+    try:
+        while cv2.waitKey(1) & 0xFF == ord('q'):
+            count += 1
+            cv2.imshow("capture", frame)
+            cv2.imwrite(".\source-jimmy\jimmy{}.jpg".foramt(count), frame)
+    except:
+        print("error accur!!")
 
     cameraCapture.release()
     cv2.destroyAllWindows()
